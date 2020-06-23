6ffae7d78652fae5d73ee1bec2f0746a5148e5a4
Michael Griff <michaelgriff55@gmail.com>
2020-06-22 20:16:27 -0400

    got rid of console log

--- client/src/utils/getHashParams.js
+++ client/src/utils/getHashParams.js
@@ -5,12 +5,8 @@ const getHashParams = (window) => {
   let e;
   let r = /([^&;=]+)=?([^&;]*)/g;
   let q = window.location.hash.substring(1);
-  console.log(window.location);
-  console.log(`q: ${q}`);
+
   while ((e = r.exec(q))) {
-    console.log(e);
-    console.log(e[1]);
-    console.log(e[2]);
     hashParams[e[1]] = decodeURIComponent(e[2]);
   }
   return hashParams;
